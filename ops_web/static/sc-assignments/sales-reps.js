cf639181deac69fe7bfba9a9d8e67f930ab8deca
William Jackson <wjackson@informatica.com>
2020-08-13 12:08:26 -0500

    Use less jQuery

--- ops_web/static/sc-assignments/sales-reps.js
+++ ops_web/static/sc-assignments/sales-reps.js
@@ -1,12 +1,7 @@
 $('#modal-edit-rep').on('show.bs.modal', function (e) {
-    let r = $(e.relatedTarget);
-    let rep_name = r.attr('data-rep-name');
-    let rep_territory = r.attr('data-rep-territory');
-    let rep_territory_name = r.attr('data-rep-territory-name');
-    let sc_employee_id = r.attr('data-sc-employee-id');
-    let modal = $(this);
-    modal.find('#edit-rep-name').text(rep_name);
-    modal.find('#edit-rep-territory').text(rep_territory);
-    modal.find('#form-edit-rep-sc-employee-id').val(sc_employee_id);
-    modal.find('#form-edit-rep-territory-name').val(rep_territory_name);
+    let r = e.relatedTarget;
+    this.querySelector('#edit-rep-name').textContent = r.dataset.repName;
+    this.querySelector('#edit-rep-territory').textContent = r.dataset.repTerritory;
+    this.querySelector('#form-edit-rep-sc-employee-id').value = r.dataset.scEmployeeId;
+    this.querySelector('#form-edit-rep-territory-name').value = r.dataset.repTerritoryName;
 });
