a9f77f015973f6193fd9da73aec51884b1d60e16
William Jackson <wjackson@informatica.com>
2020-09-15 13:39:13 -0500

    Show new scores next to current scores

--- ops_web/static/sc-competency.js
+++ ops_web/static/sc-competency.js
@@ -1,19 +1,64 @@
+window.setting_current_scores = false;
+
 document.querySelectorAll('input[type=radio]').forEach(function (el) {
     el.addEventListener('change', function () {
-        document.getElementById(`badge-${this.name}`).textContent = this.value;
+        if (window.setting_current_scores) {
+            document.getElementById(`badge-${this.name}-current`).textContent = this.value;
+            return;
+        }
+
+        // get the current score
+        let current_score = parseInt(document.getElementById(`badge-${this.name}-current`).textContent);
+        if (isNaN(current_score)) {
+            current_score = 0;
+        }
+
+        // get the new score
+        const new_score = this.value;
+
+        // show (or hide) the score delta
+        const delta_el = document.getElementById(`${this.name}-delta`);
+        const new_badge = document.getElementById(`badge-${this.name}-new`);
+        if (current_score === 0) {
+            delta_el.classList.remove('show');
+            new_badge.classList.remove('badge-danger', 'badge-success');
+            new_badge.classList.add('badge-light', 'show');
+        } else if (new_score > current_score) {
+            delta_el.classList.remove('oi-arrow-bottom');
+            delta_el.classList.add('oi-arrow-top', 'show');
+            new_badge.classList.remove('badge-danger', 'badge-light');
+            new_badge.classList.add('badge-success', 'show');
+        } else if (new_score < current_score) {
+            delta_el.classList.remove('oi-arrow-top');
+            delta_el.classList.add('oi-arrow-bottom', 'show');
+            new_badge.classList.remove('badge-light', 'badge-success');
+            new_badge.classList.add('badge-danger', 'show');
+        } else {
+            new_badge.classList.remove('show');
+        }
+
+        document.getElementById(`${this.name}-score`).textContent = new_score;
     });
 });
 
 document.getElementById('select-sc').addEventListener('change', function () {
-    // clear badge content
-    document.querySelectorAll('.badge').forEach(function (el) {
+    // clear current score content
+    document.querySelectorAll('.current-score').forEach(function (el) {
         el.textContent = '';
+    });
+
+    // hide last score stuff
+    document.getElementById('last-score').classList.remove('show');
+    document.querySelectorAll('.new-score').forEach(function (el) {
+        el.classList.remove('show');
     });
 
     // reset all score selections
     document.querySelectorAll('input[type=radio]').forEach(function (el) {
         el.checked = false;
     });
+
+    window.setting_current_scores = true;
 
     // select current scores
     const selected_option = this.options[this.selectedIndex];
@@ -34,5 +79,13 @@ document.getElementById('select-sc').addEventListener('change', function () {
         if (el) {
             el.click();
         }
+    }
+
+    window.setting_current_scores = false;
+
+    // show last score timestamp block
+    if (selected_option.dataset.scoreTimestamp) {
+        document.getElementById('last-score-span').textContent = selected_option.dataset.scoreTimestamp;
+        document.getElementById('last-score').classList.add('show');
     }
 });
